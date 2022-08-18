0f9b550a3b561dc068e1d0fe559974c5e3557d25
Roman Radakhevych <akrir@ukr.net>
2022-08-18 16:14:19 +0300

    some change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export class App extends Component {
   }
 
   componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
       localStorage.setItem(this.#LS_KEY, JSON.stringify(this.state.contacts))
     }
   }
